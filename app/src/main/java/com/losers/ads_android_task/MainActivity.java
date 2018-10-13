bf3e5d437a4ea1b0b74b66e3fe0d6990ba008bbc
deepak <deepak4u93@gmail.com>
2018-10-13 20:11:25 +0530

    add dependencies

---
+++ app/src/main/java/com/losers/ads_android_task/MainActivity.java
@@ -0,0 +1,13 @@
+package com.losers.ads_android_task;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
